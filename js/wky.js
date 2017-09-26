85416007cab368e5aecd73dd089d6e37b84cc123
wangkaiyun <975139065@qq.com>
2017-09-26 11:52:47 +0800

    xxx123

---
+++ js/wky.js
@@ -0,0 +1,8 @@
+var myswiper = new Swiper('.container',{
+    autoplay: 1000,
+    loop: true,
+    pagination: '.swiper-pagination',
+    paginationType: 'progress',
+    paginationClickable: true
+    // autoplayDisableOnInteraction: false
+})
